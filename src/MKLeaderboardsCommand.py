e8ee9ad0ae7d199c0f5577dc3498e45a3af36883
Javi <f.javier.r.m@outlook.com>
2022-07-30 01:56:43 +0200

    finish top10 table rendering

--- src/MKLeaderboardsCommand.py
+++ src/MKLeaderboardsCommand.py
@@ -1,4 +1,10 @@
+import os
+import uuid
+
+from discord import File, Embed
 from discord.ext.commands import command, Context
+from html2image import Html2Image
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 from src.MKLeaderboards import get_course
 
@@ -8,20 +14,41 @@ from src.MKLeaderboards import get_course
     help="Leaderboards"
 )
 async def leaderboards(ctx: Context, *args):
-    print(ctx.author.id)
-    print(ctx.author.name)
-    print(ctx.message)
-    print(ctx.channel)
-    print(ctx.guild)
-    print(ctx.me)
-    track = get_course(args[0])
-    response = ">>> "
-    for player in track.get('data'):
-        response += f"{str(player.get('rank')) + '.': <5} {player.get('name'):>10} {player.get('score_formatted'):^20}\n"
-    await ctx.send(response)
+    course_data = get_course(args[0])
+    track = dict(
+        name=course_data.get('track_name'),
+        image_url=f"https://www.mkleaderboards.com/images/mkw/{course_data.get('track_abbrev')}.png",
+        top=[
+            dict(
+                rank=player.get('rank'),
+                name=player.get('name'),
+                score_formatted=player.get('score_formatted')
+            ) for player in course_data.get('data')]
+    )
+    file = generate_top10_image_file(track)
+    response = Embed(title=track.get("name"), description="Top 10")
+    response.set_image(url="attachment://{}".format(file.filename))
+    await ctx.send(file=file, embed=response)
+    os.remove(file.filename)
 
 
 @leaderboards.after_invoke
 async def after_invoke(ctx: Context):
     await ctx.send(f"<@{ctx.author.id}>, results are ready!", reference=ctx.message)
 
+
+def generate_top10_image_file(track) -> File:
+    filename = 'top10_{}.png'.format(uuid.uuid4())
+    hti = Html2Image()
+    screenshot = hti.screenshot(html_str=render(track), css_file='templates/top10/top10.css', size=(700, 1000),
+                                save_as=filename)
+    return File(screenshot[0], filename)
+
+
+def render(track):
+    env = Environment(
+        loader=FileSystemLoader("templates"),
+        autoescape=select_autoescape()
+    )
+    template = env.get_template("top10/top10.html")
+    return template.render(track=track)
